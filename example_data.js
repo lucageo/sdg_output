be7614657d1a194c2d1041c366d1271542f89913
luca <luca.battistella.geo@gmail.com>
2019-06-18 13:31:13 +0200

    Add files via upload

---
+++ example_data.js
@@ -0,0 +1,134 @@
+var data = {
+  label: '1.2',
+  value: 20,
+  satellites: [
+        {
+      label: '-3',
+      value: -3,
+      tip: '',
+      satellites:[]
+    },
+        {
+      label: '-2',
+      value: -2,
+      tip: '',
+      satellites:[]
+    },
+        {
+      label: '-1',
+      value: -1,
+      tip: 'Score -1',
+      satellites:[]
+    },
+        {
+      label: 'Score 0',
+      value: 0,
+      tip: '',
+      satellites:[
+        {
+          label: '1.2',
+          tip: 'value1',
+          value: 0
+        },{
+          label: '12.4',
+          tip: 'value2',
+          value: 0
+        },{
+          label: '13.2',
+          tip: 'value3',
+          value: 0
+        },{
+          label: '14.1',
+          tip: 'value4',
+          value: 0
+        },{
+          label: '16.3',
+          tip: 'value4',
+          value: 0
+        }
+      ]
+    },
+    {
+      label: '1',
+      tip: '',
+      value: 1,
+      satellites: [
+        {
+          label: '2.4',
+          tip: 'value1',
+          value: 1
+        },{
+          label: '3.9',
+          tip: 'value2',
+          value: 1
+        },{
+          label: '6.3',
+          tip: 'value3',
+          value: 1
+        },{
+          label: '6.4',
+          tip: 'value4',
+          value: 1
+        },{
+          label: '7.2',
+          tip: 'value4',
+          value: 1
+        },{
+          label: '8.4',
+          tip: 'value4',
+          value: 1
+        },{
+          label: '9.5',
+          tip: 'value4',
+          value: 1
+        },{
+          label: '11.6',
+          tip: 'value4',
+          value: 1
+        },{
+          label: '12.5',
+          tip: 'value4',
+          value: 1
+        },{
+          label: '15.1',
+          tip: 'value4',
+          value: 1
+        }
+      ]
+    },
+    {
+      label: '2',
+      value: 2,
+      tip: '',
+      satellites:  [
+        {
+          label: '1.5',
+          tip: 'value1',
+          value: 2
+        },{
+          label: '2.3',
+          tip: 'value2',
+          value: 2
+        },{
+          label: '5.a',
+          tip: 'value3',
+          value: 2
+        },{
+          label: '6.1',
+          tip: 'value4',
+          value: 2
+        },{
+          label: '8.3',
+          tip: 'value4',
+          value: 2
+        }
+      ]
+    },
+    {
+      label: '3',
+      value: 3,
+      tip: '',
+      satellites:[]
+    }
+  ]
+};
